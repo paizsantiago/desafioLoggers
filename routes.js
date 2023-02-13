8917588a37798ddae88c702307b2e1a60a7de7ac
Santiago Paiz <paizsantiago5@gmail.com>
2023-02-13 18:57:48 -0300

    terminado

---
+++ routes.js
@@ -0,0 +1,93 @@
+const { createNProducts } = require("./faker");
+const parseArgs = require('minimist');
+const { fork } = require("child_process");
+
+const getHome = (req, res) =>{
+    const {username, password} = req.user;
+    const user = {username, password};
+    res.render("index.pug", {user: user, userExist: true})
+}
+
+const testProductosRandom = (req, res) =>{
+    let randomProducts = [];
+  createNProducts(randomProducts, 5);
+  res.render("productosFaker.pug", {title: "Productos random", products: randomProducts, productsExist: true})
+}
+
+const getLogin = (req, res) =>{
+    if(req.isAuthenticated()){
+     const {username, password} = req.user;
+     const user = {username, password};
+       res.render('index.pug' , {user: user, userExist: true})
+    } else {
+     res.render('login.pug')
+    }
+ }
+
+const postLogin = (req, res) => {
+    const {username, password} = req.user;
+    const user = {username, password};
+    const userExist = true;
+    res.render('index.pug', {user: user, userExist: userExist})
+}
+
+const getRegister =  (req, res) =>{
+    if (req.isAuthenticated()) {
+      const {username, password} = req.user;
+      const user = {username, password};
+      res.render('index.pug' , {user: user, userExist: true});
+    } else {
+      res.render('register.pug');
+    }
+}
+
+const postRegister =  (req, res) =>{
+    const {username, password} = req.body;
+    const user = {username, password};
+    res.render('index.pug', {user: user, userExist: true});
+  }
+
+const postLogout =  (req, res) => {
+    req.session.destroy((error) =>{
+        if (error) {
+            res.send("no pudo desloguear")
+        } else {
+            res.render('login.pug')
+        }
+    })
+  }
+
+const getInfo = (req, res) =>{
+    const argumentos = JSON.stringify(parseArgs(process.argv.slice(2)));
+    const path = process.argv[0];
+    const id = process.pid;
+    const version = process.version;
+    const carpeta = process.cwd();
+    const SO = process.platform;
+    const memoria = process.memoryUsage().rss;
+
+    res.render('info.pug', {argumentos: argumentos, path: path , id:id, version: version, carpeta: carpeta, SO: SO, memoria: memoria})
+}
+
+const getApiRandoms = (req, res) =>{
+    let cantidad = req.query.cantidad || 100000;
+
+    let arrayRandomNum = [];
+    for (let i = 0; i < cantidad; i++) {
+        arrayRandomNum.push(Math.floor(Math.random() * 1000) + 1);
+    }
+
+    res.JSON(arrayRandomNum)
+}
+
+module.exports = {
+    getHome,
+    testProductosRandom,
+    getLogin,
+    postLogin,
+    getRegister,
+    postRegister,
+    postLogout,
+    getInfo,
+    getApiRandoms
+}
